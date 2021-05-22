9d3b973fe966a4f53aa5e671df3604ceb047dc04
reyura <yuraa.10345.tenn1s@gmail.com>
2021-05-22 10:47:57 +0900

    Initial Commit

---
+++ SimpleTitechApp/SimpleTitechAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTitechAppApp.swift
+//  SimpleTitechApp
+//
+//  Created by Yura Aoyama on 2021/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTitechAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
